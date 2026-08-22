943bc50b587ef8d02d4ae4d37d3f349931f1e9ab
jwinkle8 <jwinkle@jhu.edu>
2026-08-21 22:54:10 -0400

    refactor CLI to reuse data retreival for streamlit app

--- weather_app.py
+++ weather_app.py
@@ -1,14 +1,5 @@
-import datetime as dt
-import requests
-import os
-from dotenv import load_dotenv
-
 from user_preferences import UserPreferences
-from weather_helpers import wind_deg_to_direction, get_temp_unit
-
-load_dotenv()
-API_KEY = os.getenv("OPENWEATHER_API_KEY")
-URL = "https://api.openweathermap.org/data/2.5/weather"
+from weather_helpers import describe_weather
 
 def modify_user_preferences(prefs: UserPreferences):
     """Save the user's preferences (in a given session) for displaying weather information."""
@@ -20,62 +11,7 @@ def modify_user_preferences(prefs: UserPreferences):
         prefs.set_verbose(verbose_input=="y")
         print("Preferences saved!")
     else:
-        print("One or more invalid inputs were entered; returning to menu with no changes to preferences")
-
-def print_verbose_weather(data: dict, prefs: UserPreferences):
-    """Print more detailed information from a successful weather request's reponse."""
-    humidity = data["main"]["humidity"]
-    print(f"    - The humidity is {humidity}%.")
-
-    wind_speed = data["wind"]["speed"]
-    wind_speed_unit = "miles/hour" if prefs.get_measurement_system() == "imperial" else "meters/second"
-    wind_deg = data["wind"]["deg"]
-    wind_direction = wind_deg_to_direction(wind_deg)
-    print(f"    - Winds are {wind_speed} {wind_speed_unit} to the {wind_direction}.")
-
-    feels_like = data["main"]["feels_like"]
-    print(f"    - The feels like temp is {feels_like}.")
-
-    low = data["main"]["temp_min"]
-    high = data["main"]["temp_max"]
-    temp_unit = get_temp_unit(prefs)
-    print(f"    - Today's high is {high}°{temp_unit} and the low is {low}°{temp_unit}.")
-
-    sunrise = dt.datetime.fromtimestamp(data["sys"]["sunrise"]).time()
-    sunset = dt.datetime.fromtimestamp(data["sys"]["sunset"]).time()
-    print(f"    - Sunrise is at {sunrise}. Sunset is at {sunset}.")
-
-def get_weather(prefs: UserPreferences):
-    """
-    Fetch weather for the given city and print it nicely.
-
-    Modifications to the default output are controlled through the argument prefs.
-    """
-    city = input("Enter a city to get the weather for: ")
-
-    params = {
-        "q": city,
-        "appid": API_KEY,
-        "units": prefs.get_measurement_system()
-    }
-    
-    response = requests.get(URL, params=params)
-    data = response.json()
-
-    if data["cod"] == "404":
-        print("No weather found for the city requested. Did you misspell it?")
-        return
-    
-    city_name = data["name"]
-    temp = data["main"]["temp"]
-    temp_unit = get_temp_unit(prefs)
-    description = data["weather"][0]["description"]
-    
-    print(f"In {city_name}, it is {temp}°{temp_unit} with {description}.")
-
-    if prefs.get_verbose():
-        print_verbose_weather(data, prefs)
-        
+        print("One or more invalid inputs were entered; returning to menu with no changes to preferences")        
 
 def main():
     """Define the primary control loop for the application."""
@@ -96,7 +32,8 @@ def main():
         user_selection = input("Enter an option: ")
 
         if user_selection == "1":
-            get_weather(prefs)
+            city = input("Enter a city to get the weather for: ")
+            print(describe_weather(city, prefs))
         elif user_selection == "2":
             modify_user_preferences(prefs)
         elif user_selection == "3":
--- weather_helpers.py
+++ weather_helpers.py
@@ -1,6 +1,73 @@
+from dotenv import load_dotenv
+import datetime as dt
+import os
+import requests
+
 from user_preferences import UserPreferences
 
-def wind_deg_to_direction(wind_deg):
+load_dotenv()
+API_KEY = os.getenv("OPENWEATHER_API_KEY")
+URL = "https://api.openweathermap.org/data/2.5/weather"
+
+def get_weather(city: str, prefs: UserPreferences):
+    """
+    Get current weather for a given city str.
+    
+    Format data according to user preference.
+    """
+    params = {
+        "q": city,
+        "appid": API_KEY,
+        "units": prefs.get_measurement_system()
+    }
+    
+    data = requests.get(URL, params=params).json()
+
+    if data["cod"] == "404":
+        return None
+    else:
+        weather_data = {
+            "city": data["name"],
+            "temp": round(data["main"]["temp"]),
+            "description": data["weather"][0]["description"],
+            "humidity": data["main"]["humidity"],
+            "wind_speed": data["wind"]["speed"],
+            "wind_speed_unit": "mi/hr" if prefs.get_measurement_system() == "imperial" else "m/s",
+            "wind_direction" : _wind_deg_to_direction(data["wind"]["deg"]),
+            "feels_like": round(data["main"]["feels_like"]),
+            "low": round(data["main"]["temp_min"]),
+            "high": round(data["main"]["temp_max"]),
+            "temp_unit": _get_temp_unit(prefs),
+            "sunrise": dt.datetime.fromtimestamp(data["sys"]["sunrise"]).time().strftime("%I:%M %p EST"),
+            "sunset": dt.datetime.fromtimestamp(data["sys"]["sunset"]).time().strftime("%I:%M %p EST")
+        }
+        return weather_data
+
+def describe_weather(city: str, prefs: UserPreferences):
+    """
+    Fetch weather for the given city and print it nicely.
+
+    Modifications to the default output are controlled through the argument prefs.
+    """
+    data = get_weather(city, prefs)
+
+    weather_str = "\n"
+    if data is None:
+        return weather_str + "No weather found for the city requested. Did you misspell it?"
+    else:
+        weather_str += f"In {data['city']}, it is {data['temp']}°{data['temp_unit']} with {data['description']}."
+
+        if prefs.get_verbose():
+            weather_str += ("\n" + f"""Here are some more deatils about today's weather:
+    - The humidity is {data['humidity']}%.
+    - Winds are {data['wind_speed']} {data['wind_speed_unit']} to the {data['wind_direction']}.
+    - The feels like temp is {data['feels_like']}°{data['temp_unit']}.
+    - Today's high is {data['high']}°{data['temp_unit']} and the low is {data['low']}°{data['temp_unit']}.
+    - Sunrise is at {data['sunrise']} EST. Sunset is at {data['sunset']} EST.""")
+
+        return weather_str
+
+def _wind_deg_to_direction(wind_deg):
     """Convert a meteorological measure of the wind direction in degrees to a descriptive string."""
     if wind_deg >= 22.5 and wind_deg < 67.5:
         return "Northeast"
@@ -19,7 +86,7 @@ def wind_deg_to_direction(wind_deg):
     else:
         return "North"
 
-def get_temp_unit(prefs: UserPreferences):
+def _get_temp_unit(prefs: UserPreferences):
     """Return the temperature unit for the user's preferred measurement system."""
     measurement_system = prefs.get_measurement_system()
     if measurement_system == "standard":
